1842e2471c59d12375759f54e16b60788be97d2c
LeoTian0000 <zhangzhongtian1@gmail.com>
2018-04-23 12:11:35 +0800

    Finish!

--- app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
@@ -2,12 +2,19 @@ package sg.edu.rp.c346.demoimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView2, imageView3, imageView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView2.setImageResource(R.drawable.day2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView3.setImageResource(R.drawable.day3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView4.setImageResource(R.drawable.day4);
     }
 }
